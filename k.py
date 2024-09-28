d2593b3d89587bf410aa9b5bf94df00b84bdc3c6
hjqx <180844972+hjqx@users.noreply.github.com>
2024-09-29 00:02:04 +0200

    Add files via upload

---
+++ k.py
@@ -0,0 +1,24 @@
+{     "Made by zqh aka hjqx":
+ "https://github.com/hjqx/DOJZ",
+      "Project Dkz27": "v1.0"}
+
+(_:=(_:=
+  __builtins__)and (
+      __ := lambda __d,__w,à : 
+(lambda _, __: __(__(_, __d)(__w), à)(
+   1, 10))) and (__ := (lambda _ : 
+                        
+__(_['\x64'],_['c'],_['r']))((___:=
+{"d":(d :=lambda __,_: ((_*__).
+__add__("\x69mp%s")+_*__)%'{}t'.
+format('%sr'%'o')) and (d:=d(2,"_")),
+"c":(c:=(lambda r, d  :(lambda _,__
+ : (f"{_}zx%s"%__[::r]).replace(d[::r]
+                                
+
+,'n\x64'))("r\x61","mo"))(-0x1,"\x78z"))
+, "r":(lambda z: f"%snd{z[::-1]}"
+%'r\x61')('tn\x69')}))) and (__( _, getattr)))
+
+
+print(_)
