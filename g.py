329b97b1da53f8a486ae482053e728c5bbf41484
hjqx <180844972+hjqx@users.noreply.github.com>
2024-09-29 00:08:02 +0200

    Add files via upload

---
+++ g.py
@@ -0,0 +1,13 @@
+def _7vzl020(__892,_s0x82):
+    def _0x83728ç939(___, ____, e: int = __builtins__):
+        return lambda _, __: getattr(  # DZKZKZ
+         e, "__imp%s" % ("__tr" + "o")[::-0x1]  # 2Z883
+        )("random")  
+    ___7383 = lambda: _0x83728ç939  # 988338
+    _3 = lambda: getattr(  # 83838
+        ((___7383())(0x837, 0x_2872))("SssssSSSsSSssSSssS", "_0X_37737"),
+        dir(((___7383())(object, 0x83_37))(__build_class__, "0x837"))[0x3a],
+    )(0x_922,0x_2833)
+    return(_3)()
+_S=lambda __E:_7vzl020(__E,globals())
+print(_S(3973))
